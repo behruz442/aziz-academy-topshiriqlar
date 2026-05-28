0872d0f6196243f661552feed601bc1d9197135d
behruz442 <behruzrozmatov79@gmail.com>
2026-05-28 18:13:36 +0500

    Add: 3) Birinchi va oxirgi belgi [behruz442]

---
+++ lms_kod_topshiriqlari/Ma_lumot_turlari__str__string__kirish/3__Birinchi_va_oxirgi_belgi.py
@@ -0,0 +1,10 @@
+text = input().strip()
+
+if text:
+    first_char = text[0]
+    last_char = text[-1]
+    
+    print(f"First: {first_char}")
+    print(f"Last: {last_char}")
+else:
+    print("Satr bo'sh")
