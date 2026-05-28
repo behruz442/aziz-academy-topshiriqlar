62f6a839a306f02627ef1ee3f3907f53db010b53
behruz442 <behruzrozmatov79@gmail.com>
2026-05-28 18:21:35 +0500

    Add AI task: Savdo markazi narx hisoblagichi

---
+++ ai_topshiriqlar/Savdo_markazi_narx_hisoblagichi.py
@@ -0,0 +1,13 @@
+# Savdo markazi narx hisoblagichi
+# Kurs: IT Dasturlash
+# Mavzu: 1-mavzu: Python va dasturlashga kirish
+# Ball: 50
+# Aziz Academy — AI Topshiriq
+
+narx = int(input())
+soni= int(input())
+foiz = int(input())
+
+jami = narx + (narx * foiz // 100) * soni
+
+print(jami)
