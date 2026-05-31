9e2e7b308cd531f85d742232ff9c76edfd73578c
behruz442 <behruzrozmatov79@gmail.com>
2026-05-31 11:05:56 +0500

    Add: 7) Eng past baho [behruz442]

---
+++ lms_kod_topshiriqlari/List_of_dicts__o_quvchilar_va_mahsulotlar/7__Eng_past_baho.py
@@ -0,0 +1,7 @@
+n = int(input())
+students = []
+for _ in range(n):
+    name, score = input().split()
+    students.append({'name': name, 'score': int(score)})
+min_score = min(s['score'] for s in students)
+print(min_score)
