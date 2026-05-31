e1874875c6525db5aac19ed26be5225ff84ed60a
behruz442 <behruzrozmatov79@gmail.com>
2026-05-31 11:19:15 +0500

    Add: 5) Unikal o‘quvchilar soni (hamma kurslar bo‘yicha) [behruz442]

---
+++ lms_kod_topshiriqlari/Nested_structures__dict_ichida_list__list_ichida_d/5__Unikal_o_quvchilar_soni__hamma_kurslar_bo_yicha.py
@@ -0,0 +1,10 @@
+n = int(input().strip())
+courses = []
+for _ in range(n):
+    parts = input().split()
+    name = parts[0]
+    k = int(parts[1])
+    students = parts[2:2+k]
+    courses.append({'name': name, 'students': students})
+result = len({student for course in courses for student in course['students']})
+print(result)
