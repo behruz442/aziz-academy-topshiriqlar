8ee52e0b4f75b1cc8f98d6620b2686aabc99bd0d
behruz442 <behruzrozmatov79@gmail.com>
2026-05-31 11:45:30 +0500

    Add: 15) 1..n ichidan 'FizzBuzz' label (if-else mapping) [behruz442]

---
+++ lms_kod_topshiriqlari/List_comprehensions___shartli/15__1__n_ichidan__FizzBuzz__label__if-else_mapping.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+result = []
+for x in range(1, n+1):
+    if x % 3 == 0 and x % 5 == 0:
+        result.append("FizzBuzz")
+    elif x % 3 == 0:
+        result. append("Fizz")
+    elif x % 5 == 0:
+        result.append("Buzz")
+    else:
+        result.append(str(x))
+print(' '.join(result))
