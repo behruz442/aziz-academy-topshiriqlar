d72108860aea2054ac6f64f24512753d419f1e20
behruz442 <behruzrozmatov79@gmail.com>
2026-05-31 11:26:52 +0500

    Add: 13) Har userda nechta tag bor? (dict ichida list) [behruz442]

---
+++ lms_kod_topshiriqlari/Nested_structures__dict_ichida_list__list_ichida_d/13__Har_userda_nechta_tag_bor___dict_ichida_list_.py
@@ -0,0 +1,9 @@
+n = int(input().strip())
+users = []
+for _ in range(n):
+    parts = input().split()
+    username = parts[0]
+    k = int(parts[1])
+    tags = parts[2:2+k]
+    users.append({'username': username, 'tags': tags})
+    print(username, len(tags))
