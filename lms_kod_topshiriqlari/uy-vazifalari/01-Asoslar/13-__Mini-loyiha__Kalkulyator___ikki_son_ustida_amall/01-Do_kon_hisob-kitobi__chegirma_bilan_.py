4cf08a64044fcc66f96f3a119c024678558fab92
behruz442 <behruzrozmatov79@gmail.com>
2026-07-02 12:23:09 +0500

    Add: Do'kon hisob-kitobi (chegirma bilan) [behruz442]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/13-__Mini-loyiha__Kalkulyator___ikki_son_ustida_amall/01-Do_kon_hisob-kitobi__chegirma_bilan_.py
@@ -0,0 +1,9 @@
+soni = int(input())
+narxi = int(input())
+chegirma_foizi = int(input())
+jami = soni * narxi
+chegirma_summasi = (jami * chegirma_foizi) // 100
+yakuniy_narx = jami - chegirma_summasi
+print(jami)
+print(chegirma_summasi)
+print(yakuniy_narx)
